458961e2d2f25783e4d6c120524e521d49a121fa
geocipher <geocipher@571381a0-0f4c-11df-8a19-6d7f5c3cf6d3>
2010-02-01 17:01:15 +0000

    Added parCer32

---
+++ trunk/parCer32/parCer32/ParseTree.cs
@@ -0,0 +1,116 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace parCer32
+{
+    #region ParseTree
+    public class ParseErrors : List<ParseError>
+    {
+    }
+
+    public class ParseError
+    {
+        private string message;
+        private int code;
+        private int line;
+        private int col;
+        private int pos;
+        private int length;
+
+        public int Code { get { return code; } }
+        public int Line { get { return line; } }
+        public int Column { get { return col; } }
+        public int Position { get { return pos; } }
+        public int Length { get { return length; } }
+        public string Message { get { return message; } }
+
+        public ParseError(string message, int code, ParseNode node) : this(message, code,  0, node.Token.StartPos, node.Token.StartPos, node.Token.Length)
+        {
+        }
+
+        public ParseError(string message, int code, int line, int col, int pos, int length)
+        {
+            this.message = message;
+            this.code = code;
+            this.line = line;
+            this.col = col;
+            this.pos = pos;
+            this.length = length;
+        }
+    }
+
+    
+    public partial class ParseTree : ParseNode
+    {
+        public ParseErrors Errors;
+
+        public List<Token> Skipped;
+
+        public ParseTree() : base(new Token(), "ParseTree")
+        {
+            Token.Type = TokenType.Start;
+            Token.Text = "Root";
+            Skipped = new List<Token>();
+            Errors = new ParseErrors();
+        }
+
+        public string PrintTree()
+        {
+            StringBuilder sb = new StringBuilder();
+            int indent = 0;
+            PrintNode(sb, this, indent);
+            return sb.ToString();
+        }
+
+        private void PrintNode(StringBuilder sb, ParseNode node, int indent)
+        {
+            
+            string space = "".PadLeft(indent, ' ');
+
+            sb.Append(space);
+            sb.AppendLine(node.Text);
+
+            foreach (ParseNode n in node.Nodes)
+                PrintNode(sb, n, indent + 2);
+        }
+        
+        
+        
+        
+        
+        
+    }
+
+    public partial class ParseNode
+    {
+        protected string text;
+        protected List<ParseNode> nodes;
+        
+        public List<ParseNode> Nodes { get {return nodes;} }
+        
+        public ParseNode Parent;
+        public Token Token; 
+        public string Text { 
+            get { return text;} 
+            set { text = value; }
+        } 
+
+        public virtual ParseNode CreateNode(Token token, string text)
+        {
+            ParseNode node = new ParseNode(token, text);
+            node.Parent = this;
+            return node;
+        }
+
+        protected ParseNode(Token token, string text)
+        {
+            this.Token = token;
+            this.text = text;
+            this.nodes = new List<ParseNode>();
+        }
+
+    }
+    
+    #endregion ParseTree
+}
