3f77eaf7f75c35c67c2edc40ee6f1e191002032d
blaqfire08 <blaqfire08@571381a0-0f4c-11df-8a19-6d7f5c3cf6d3>
2010-02-01 17:29:37 +0000

    Added test.c

---
+++ trunk/TPG/test.c
@@ -0,0 +1,118 @@
+#include<stdio.h>
+int x,t=2,c,f,g,e=2;int y;
+float we=2;
+
+x=2;d=2;
+a=1;
+
+int main(int x,double b)
+{	
+	int y=2,r,t,w,e;
+	z=4;
+
+	if(x==2)
+	{
+		int x=2,c=2,d,a;
+		p=2;
+	   switch(dsd)
+	   {
+          case 3:  
+          b = 5+2/4;
+          
+          default:
+          int c =5;
+          }
+
+	}
+
+	switch(q)
+	{
+	case 1:      
+         a = 5;
+	  if(x<=2)
+	  {
+		x=2;
+	  }
+       case 2:
+          switch(dsd)
+	   {
+          case 3:  
+          b = 5+2/4;
+          
+          default:
+          int c =5;
+          }
+       }
+
+
+	if(x==2)
+	  x=23;
+	if(x<=2)
+	{
+		x=23;
+		fd=233;
+	}
+	else
+	if(m!=90)
+		k=2;
+	else
+	  oo=9999;
+
+
+	if(xxx!=111)
+		xxx=69;
+	else
+	{
+	 if(xxx!=222)	 
+	 	xxx=88;
+	} 
+       
+	p=0;
+
+	for(ulonglong ctr=23;x>9;x++)
+	{
+		x=2;
+		while(m!=0)
+		{
+		 m=554545;
+		}
+	}
+
+	while(x==8||jjj<=23)
+	{
+	  char lklskdk=23287382;
+	  if((x<2)&&y<2||(x!=x))
+		lll=999;
+	  //if(){}
+	}
+
+	for(;;){}
+	for(x=2;;){}
+	for(;x>=2;){}
+	for(;;x--){}
+
+	do
+	{
+	 x=2;
+	}
+	while(x>0);
+
+	while(x==90)
+	{
+	 x=2;
+	}
+
+	printf("hello",x);
+	scanf("%d",&x);
+
+return v;
+return 2;
+return x+2;
+return g+k;	
+return (j+k);
+return (2-s*f-(g+p));
+return f*(g);
+}
+
+
+
