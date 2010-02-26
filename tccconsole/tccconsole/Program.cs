639f163ce49ff65f9f007782b6916ce00b8a850f
geocipher <geocipher@571381a0-0f4c-11df-8a19-6d7f5c3cf6d3>
2010-02-26 21:39:51 +0000

    Manually updated tccconsole Program.cs and Scanner.cs

--- tccconsole/tccconsole/Program.cs
+++ tccconsole/tccconsole/Program.cs
@@ -41,12 +41,21 @@ namespace tccconsole
                 //tok = scanner.Scan(scanner.Tokens.ToArray());
                 tok = scanner.Scan();
                 //Here yay
-                if (tok.Type != TokenType.WHITESPACE)
+                if (tok.Type != TokenType.WHITESPACE && tok.Type != TokenType.COMMENTLINE && tok.Type!= TokenType.COMMENTBLOCK)
 
                 {
                     ConsoleColor.SetForeGroundColour(ConsoleColor.ForeGroundColour.Green, true);
-                    Console.WriteLine("Token: {0} \n Type: {1,-12} Line: {2,3} \n", tok.Text, tok.Type, tok.LinePos);
-                    fstream.WriteLine("Token: {0} \r\n Type: {1,-12} Line: {2,3} \r\n", tok.Text, tok.Type, tok.LinePos);
+                    if (tok.Type == TokenType.NULL)
+                    {
+                        Console.WriteLine("ERROR: Unknown Token found at Line: {2,3} \n", tok.Text.Trim(), "Unknown", tok.LinePos);
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Token: {0} \n Type: {1,-12} Line: {2,3} \n", tok.Text.Trim(), tok.Type, tok.LinePos);
+                        fstream.WriteLine("Token: {0} \r\n Type: {1,-12} Line: {2,3} \r\n", tok.Text.Trim(), tok.Type, tok.LinePos);
+                    }
+
                     ConsoleColor.SetForeGroundColour();
                 }
             } while (input.Length != tok.EndPos);
